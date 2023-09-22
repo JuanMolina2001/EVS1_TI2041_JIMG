c19a73cd8b4c061004574195d9b48e1a822056dc
juan molina <juanmolinajm2001@gmail.com>
2023-09-22 12:32:08 -0300

    trabajo final 3

--- jimg_proyecto1/asgi.py
+++ jimg_proyecto1/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for proyecto project.
+ASGI config for jimg_proyecto1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jimg_proyecto1.settings')
 
 application = get_asgi_application()
